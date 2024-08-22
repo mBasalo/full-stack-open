5a7ea3971af410494f8852d5f52bbbb94b243b23
Mauricio Basalo <mbasalogutierrez@gmail.com>
2024-08-22 15:40:44 -0300

    1.4

--- part1/src/App.jsx
+++ part1/src/App.jsx
@@ -4,11 +4,20 @@ import Total from './Components/Total';
 
 const App = () => {
   const course = 'Half Stack application development';
-
+  
   const parts = [
-    { name: 'Fundamentals of React', exercises: 10 },
-    { name: 'Using props to pass data', exercises: 7 },
-    { name: 'State of a component', exercises: 14 }
+    {
+      name: 'Fundamentals of React',
+      exercises: 10
+    },
+    {
+      name: 'Using props to pass data',
+      exercises: 7
+    },
+    {
+      name: 'State of a component',
+      exercises: 14
+    }
   ];
 
   return (
