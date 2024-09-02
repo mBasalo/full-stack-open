74acd75c2cef52288c1dda7f6d579dba5c41b5e9
Mauricio Basalo <mbasalogutierrez@gmail.com>
2024-09-02 14:29:55 -0300

    1.7

--- part1/src/App.jsx
+++ part1/src/App.jsx
@@ -1,15 +1,19 @@
 import { useState } from "react";
 
 const App = () => {
-  // Definimos los estados para cada tipo de feedback
   const [good, setGood] = useState(0);
   const [neutral, setNeutral] = useState(0);
   const [bad, setBad] = useState(0);
 
-  // Funciones para manejar los clics en cada botón
   const handleGoodClick = () => setGood(good + 1);
   const handleNeutralClick = () => setNeutral(neutral + 1);
   const handleBadClick = () => setBad(bad + 1);
+
+  const total = good + neutral + bad;
+  const average = total > 0 ? (good - bad) / total : 0; 
+  const positivePercentage = total > 0 ? (good / total) * 100 : 0;
+
+
 
   const handleReset = () => {
     setGood(0);
@@ -21,21 +25,23 @@ const App = () => {
     <>
       <h1>give feedback</h1>
 
-      {/* Asignamos las funciones a los eventos onClick de los botones */}
       <button onClick={handleGoodClick}>good</button>
       <button onClick={handleNeutralClick}>neutral</button>
       <button onClick={handleBadClick}>bad</button>
 
       <h2>statistics</h2>
 
-      {/* Mostramos los resultados */}
       <p>good: {good}</p>
       <p>neutral: {neutral}</p>
       <p>bad: {bad}</p>
       <br />
+
+      <p>total: {total}</p>
+      <p>average: {average.toFixed(2)}</p>
+      <p>positive feedback: {positivePercentage.toFixed(2)}%</p>
+      <br />
+
       <button onClick={handleReset}>reset</button>
-
-
     </>
   );
 };
