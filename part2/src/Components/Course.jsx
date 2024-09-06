8cca224318460161ba7ff6ddf09b79f5754c92d2
Mauricio Basalo <mbasalogutierrez@gmail.com>
2024-09-06 11:30:20 -0300

    2.1

---
+++ part2/src/Components/Course.jsx
@@ -0,0 +1,18 @@
+import Header from './Header'
+import Content from './Content'
+
+const Course = ({ course }) => {
+
+  console.log(course);
+  console.log("hola mundo");
+  
+  
+  return (
+    <div>
+      <Header courseName={course.name} />
+      <Content parts={course.parts} />
+    </div>
+  )
+}
+
+export default Course
---
+++ part2/src/Components/Header.jsx
@@ -0,0 +1,6 @@
+const Header = ({ courseName }) => {
+    return <h1>{courseName}</h1>
+  }
+  
+  export default Header
+  
